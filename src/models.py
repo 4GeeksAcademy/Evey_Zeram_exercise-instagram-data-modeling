4ce55084d9454681aa23b684d87f26ce83442d0f
Evelyn <130219194+EveyZeram@users.noreply.github.com>
2024-01-09 19:02:08 +0000

    EveyZeram ig done

--- src/models.py
+++ src/models.py
@@ -1,37 +1,61 @@
+from eralchemy2 import render_er
+
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, Table, Date, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
+
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+# OPCION 1: con relaciones dentro de las tablas
+
+# Crear tabla: user, posts, media, follower y comment.
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    lastname = Column(String(10))
+    firstname = Column(String(10))
+    email = Column(String(30), nullable=False)
+    password = Column(String(10), nullable=False)
+    subscription_date = Column(Date)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key = True)
+    user_id = Column(Integer, ForeignKey(User.id))
+    user = relationship(User)      
 
-    def to_dict(self):
-        return {}
+   
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key = True)
+    comment_text = Column(String, nullable=False)
+    author_id = Column(Integer, ForeignKey(User.id))
+    user = relationship(User)
+    post_id = Column(Integer, ForeignKey(Post.id))
+    post = relationship(Post)
 
-## Draw from SQLAlchemy base
-try:
-    result = render_er(Base, 'diagram.png')
-    print("Success! Check the diagram.png file")
-except Exception as e:
-    print("There was a problem genering the diagram")
-    raise e
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key = True)
+    type = Column(Enum, nullable=False)
+    url = Column(String)
+    post_id = Column(Integer, ForeignKey(Post.id))
+    post = relationship(Post)
+
+# Usar backref para varias to varios. 
+class Follower(Base):
+    __tablename__ = 'follower'
+    user_from_id = Column(Integer, ForeignKey(User.id), primary_key=True)
+    user_from = relationship(User, foreign_keys=[user_from_id], backref="followers_from")
+    user_to_id = Column(Integer, ForeignKey(User.id), primary_key=True)
+    user_to = relationship(User, foreign_keys=[user_to_id], backref="followers_to")
+
+render_er(Base, 'diagram.png')
